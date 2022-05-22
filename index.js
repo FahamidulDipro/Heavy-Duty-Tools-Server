5da097f0a5908ae69df206359a749626093ab123
FahamidulDipro <mdfahamidulislam047@gmail.com>
2022-05-22 18:49:29 +0600

    database connected and tools api created

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const res = require('express/lib/response');
 const app = express()
 const port = process.env.PORT || 5000;
 
@@ -8,9 +10,29 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+//Checking the server
 app.get('/', (req, res) => {
   res.send('Heavy Duty Tool Server Working!')
 })
+
+
+//Database Connection
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.6nnj1.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+  await client.connect();
+  const toolsCollection = client.db("heavy-duty-tools-db").collection("tools");
+app.get('/tools',async (req,res)=>{
+  const tools = await toolsCollection.find().toArray();
+  res.send(tools);
+})
+
+  
+}
+run().catch(console.dir);
+
 
 app.listen(port, () => {
   console.log(`Listenting to port ${port}`)
