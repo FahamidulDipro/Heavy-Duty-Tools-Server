e3d0fdf1565f231258ebd50d79f5af063463f822
FahamidulDipro <mdfahamidulislam047@gmail.com>
2022-05-25 13:16:38 +0600

    Payment intent api created for getting the amount and client secret

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const res = require("express/lib/response");
 const app = express();
 const port = process.env.PORT || 5000;
 const jwt = require("jsonwebtoken");
-
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 app.use(cors());
 app.use(express.json());
 
@@ -139,6 +139,22 @@ async function run() {
     console.log(id);
     const order = await orderCollection.findOne(query);
     res.send(order);
+  });
+  //Creating Payment
+  app.post("/create_payment_intent", verifyJWT, async (req, res) => {
+    const price = req.body.totalAmountPay;
+    console.log(price);
+    const amount = price * 100;
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: amount,
+      currency: "usd",
+      automatic_payment_methods: {
+        enabled: true,
+      },
+    });
+    res.send({
+      clientSecret: paymentIntent.client_secret,
+    });
   });
 }
 run().catch(console.dir);
